a3f38c3025665dda0eab468b2deb6ceef44011d8
mouaz43 <mouazmjr51@icloud.com>
2025-08-31 21:52:30 +0200

    Update main.tsx
    mount App + styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // <-- Tailwind entry & custom CSS
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
