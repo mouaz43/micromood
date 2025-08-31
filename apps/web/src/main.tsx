0ce12b17226555c5006765899a949df219b890e9
mouaz43 <mouazmjr51@icloud.com>
2025-08-31 05:00:29 +0200

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,6 @@
-// apps/web/src/main.tsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // ✅ default export
-
+import App from "./App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
