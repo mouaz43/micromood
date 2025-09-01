67f31f52bba338a5c4f009ef44a695d3d943ad00
mouaz43 <mouazmjr51@icloud.com>
2025-09-01 06:30:26 +0200

    Update prisma.ts

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,2 +1,14 @@
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'production' ? ['warn', 'error'] : ['query', 'info', 'warn', 'error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
