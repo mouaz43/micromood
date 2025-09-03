4ac64a4e20688d230e056bb40ddcf3eb8c51926f
mouaz43 <mouazmjr51@icloud.com>
2025-09-03 06:44:31 +0200

    Delete apps directory

--- apps/web/src/components/SiteFooter.tsx
+++
@@ -1,7 +0,0 @@
-export function SiteFooter() {
-  return (
-    <footer className="mt-10 text-xs opacity-70 text-center">
-      Built by <span className="font-medium">Mouaz Almjarkesh</span>
-    </footer>
-  );
-}
--- apps/web/src/components/TopNav.tsx
+++
@@ -1,8 +0,0 @@
-export default function TopNav() {
-  return (
-    <header className="topnav">
-      <div className="brand"><span className="glyph">◐</span> <span>MICROMOOD</span></div>
-      <div className="built">Built by Mouaz Almjarkesh</div>
-    </header>
-  );
-}
