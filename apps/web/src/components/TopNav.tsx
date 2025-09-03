db9501eddc24f92f2bb160603b385f4576270287
mouaz43 <mouazmjr51@icloud.com>
2025-09-03 05:17:26 +0200

    Update TopNav.tsx
    feat(web): clean brand bar

--- apps/web/src/components/TopNav.tsx
+++ apps/web/src/components/TopNav.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-
 export default function TopNav() {
   return (
     <header className="topnav">
-      <div className="brand">
-        <span className="logo">◐</span>
-        <span className="name">MicroMood</span>
-      </div>
+      <div className="brand"><span className="glyph">◐</span> <span>MICROMOOD</span></div>
       <div className="built">Built by Mouaz Almjarkesh</div>
     </header>
   );
